d1a74eff2a95eac037d16c6ce4171c44d953af8d
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2025-09-14 20:51:55 +0600

    add find problems

--- find.js
+++ find.js
@@ -7,6 +7,38 @@ const greaterThan50 = numbers.find( num => num > 4)
 console.log(greaterThan50);
 
 
+// From an array of numbers, find the first number that is divisible by 7. 
+let divisibleBySeven = numbers.find(num => num % 7 === 0);
+console.log(divisibleBySeven);
+
+
+/*
+Given [3, 7, 12, 19, 21], find the first number greater than 10.
+→ Expected: 12
+*/ 
+let numbersNew = [3, 7, 12, 19, 21];
+let firstNumGreaterThanTen = numbersNew.find(number => number > 10);
+console.log(firstNumGreaterThanTen);
+
+
+/*
+Given ["cat", "dog", "elephant", "fish"], find the first word with more than 3 letters.
+→ Expected: "elephant"
+*/
+let fishes = ["cat", "dog", "elephant", "fish"];
+let wordMoreThanThreeChar = fishes.find(fish => fish.length > 3);
+console.log(wordMoreThanThreeChar);
+
+
+/*
+Given [1, 2, 3, 4, 5], find the first odd number.
+→ Expected: 1
+*/ 
+let numbersNewArr = [1, 2, 3, 4, 5];
+let oddNumbers = numbersNewArr.find(number => number % 2 !== 0);
+console.log(oddNumbers);
+
+
 /*
 const users = [
     { id: 1, name: "Alice", age: 25, city: "New York" },
@@ -26,3 +58,37 @@ const users = [
 ];
 let userId = users.find(user => user.id === 3);
 console.log(userId);
+
+
+/*
+const students = [
+    { id: 1, name: "David", grade: "A" },
+    { id: 2, name: "Eve", grade: "C" },
+    { id: 3, name: "Frank", grade: "B" }
+];
+Task: Find the first student whose grade is "B".
+*/ 
+const students = [
+    { id: 1, name: "David", grade: "A" },
+    { id: 2, name: "Eve", grade: "C" },
+    { id: 3, name: "Frank", grade: "B" }
+];
+let firstStudentGetB = students.find(student => student.grade === "B");
+console.log(firstStudentGetB);
+
+
+/*
+const movies = [
+    { id: 1, title: "Inception", rating: 8.8 },
+    { id: 2, title: "The Room", rating: 3.6 },
+    { id: 3, title: "Interstellar", rating: 8.6 }
+];
+Task: Find the first movie that has a rating below 5.
+*/ 
+const movies = [
+    { id: 1, title: "Inception", rating: 8.8 },
+    { id: 2, title: "The Room", rating: 3.6 },
+    { id: 3, title: "Interstellar", rating: 8.6 }
+];
+let ratingBelowFive = movies.find(movie => movie.rating < 5);
+console.log(ratingBelowFive);
